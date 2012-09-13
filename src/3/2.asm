b0d9b96d5550b791d053af8d869bd9e6152c16d1
Kirill <fishbone1319@gmail.com>
2012-09-14 00:10:18 +0400

    part two for variant 13 and variant 3

---
+++ src/3/2.asm
@@ -0,0 +1,36 @@
+.model  small
+.code
+.stack 128
+
+main:
+
+
+mov	ax, 10h
+int	10h	
+
+MOV  DX,3C4H        ;указываем на адресный регистр
+MOV  AL,2           ;индекс регистра маски карты
+OUT  DX,AL          ;установка адреса
+INC  DX             ;указываем на регистр данных
+MOV  AL, 1001b      ;код цвета
+OUT  DX,AL          ;посылаем код цвета
+
+mov	ax, 0a000h
+mov es, ax				
+
+mov di, 05h
+
+mov es:[di], 0ffh 
+
+mov di, 55h
+
+mov es:[di], 9h 
+
+
+xor	ax, ax				;ожидание нажатия клавиши
+int	16h
+
+mov	ax,4c00h			;выход из графики с возвратом
+int	21h				   ;в предыдущий режим
+
+end main
